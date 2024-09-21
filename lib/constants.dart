fda26e391f009430ee72cf7983f8c59908f544fe
Longngoo <100961248+but14@users.noreply.github.com>
2024-09-21 14:36:26 +0700

    code 2109

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class Constants {
+  static var primaryColor = const Color(0xff296e48);
+
+
+  // Onboarding texts
+  static var titleOne = "Learn more about plants";
+  static var titleTwo = "Discover new species";
+  static var descriptionOne = "Read how to care.";
+  static var titleThree = "Plant a tree, green the Earth";
+  static var descriptionTwo = "Are you a plant lover? Connect with other plant lovers.";
+  static var descriptionThree = "Find almost all types of plants that you like here.";
+}
+
